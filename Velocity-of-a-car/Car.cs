8e42fddc158eb3041f62d13bebb256d39e4329bf
odeth <odeth@AYELLEN>
2024-02-18 17:18:12 -0600

    Agregar archivos de proyecto.

---
+++ Velocity-of-a-car/Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Velocity_of_a_car
+{
+	internal class Car
+	{
+		//Properties 
+		protected double distence;
+
+		public double Distance 
+		{
+			get { return distence; }
+			set { distence = value; }
+		}
+
+		protected double time;
+
+		public double Time
+		{
+			get { return time; }
+			set { time = value; }
+		}
+
+		//Constructor 
+
+		public Car() 
+		{ 
+			time = 0;
+			distence = 0;	
+		}
+
+
+
+		//Method 
+		public double GetSpeed() 
+		{
+		return Distance/time;
+		}
+
+			
+
+	}
+}
---
+++ Velocity-of-a-car/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Velocity_of_a_car
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car Car1= new Car();
+            Car1.Time = 20;
+            Car1.Distance = 10;
+            Console.WriteLine("Distance:" + Car1.Distance);
+            Console.WriteLine("Distance:" + Car1.Time);
+            Console.WriteLine("The speed of the car is:" + Car1.GetSpeed()+" m/s");
+
+        }
+    }
+}
